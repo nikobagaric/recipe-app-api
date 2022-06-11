aa3346c7e814994d81d9cd4a43cf0a1050beb3bd
Null0Desgin <nulldesign.contact@gmail.com>
2022-06-11 14:29:09 +0200

    Added required email input

--- app/core/models.py
+++ app/core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from multiprocessing.sharedctypes import Value
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -14,6 +15,8 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_field):
         """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
         user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
         user.save(using=self._db)
