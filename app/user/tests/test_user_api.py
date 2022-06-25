bf50e158791e37ef8b20fb9aebae9e6d21009f48
Null0Desgin <nulldesign.contact@gmail.com>
2022-06-25 13:02:34 +0200

    Fixed tests for user token

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -86,11 +86,19 @@ class PublicUserApiTests(TestCase):
     def test_create_token_bad_credentials(self):
         """"Test returns error if credentials are invalid"""
         create_user(email='test@example.com', password='goodpass')
-        
+
         payload = {'email': 'test@example.com', 'password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('token', res.data)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': 'pass123'}
+        res = self.client.post(TOKEN_URL, payload)
+
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
