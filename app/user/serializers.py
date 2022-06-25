08fdd5f92b680ea4f82b8042b9aeb95f4d05bbd0
Null0Desgin <nulldesign.contact@gmail.com>
2022-06-25 14:18:17 +0200

    Added tests for manage user

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -33,7 +33,7 @@ class AuthTokenSerializer(serializers.Serializer):
 
     def validate(self, attrs):
         """Validate and authenticate the user."""
-        email=attrs.get('email')
+        email = attrs.get('email')
         password = attrs.get('password')
         user = authenticate(
             request=self.context.get('request'),
