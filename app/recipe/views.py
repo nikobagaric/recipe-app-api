8b862296d90bda4411750ff42ee5c394c5233225
Null0Desgin <nulldesign.contact@gmail.com>
2022-07-03 20:17:13 +0200

    Added update and delete for tags

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class TagViewSet(mixins.UpdateModelMixin,
+                 mixins.DestroyModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """View for managing tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
